6d82bbab00ff4ae3e51037726f011b09fdb6fc94
smp1234 <40439382+smp1234@users.noreply.github.com>
2018-06-28 15:52:57 -0400

    Code added for integration of java and python. Settings are static needs to be changed.

--- RecipeFinder (BackEnd)/recipefinder/src/main/java/com/recipefinder/service/RecipeService.java
+++ RecipeFinder (BackEnd)/recipefinder/src/main/java/com/recipefinder/service/RecipeService.java
@@ -1,6 +1,9 @@
 package com.recipefinder.service;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class RecipeService {
 
@@ -22,7 +25,19 @@ public class RecipeService {
 	public String predictRecipe(String fileName) {
 		
 		//call prediction model to get recipe name
-		
+		Process p;
+		try {
+			String path = "C:\\Users\\spatel\\Desktop\\project.py";
+			String imagePath = "C:/Users/spatel/Desktop/chickenb1.jpg";
+			ProcessBuilder pb = new ProcessBuilder(Arrays.asList("python", "project.py","./images/chickenb1.jpg"));
+			p = pb.start();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			System.out.println("Answer: " + in.readLine());
+			p.destroy();
+		} catch (Exception e) {
+			System.out.println(e);
+		} 
 		return null;
 	}
 	
