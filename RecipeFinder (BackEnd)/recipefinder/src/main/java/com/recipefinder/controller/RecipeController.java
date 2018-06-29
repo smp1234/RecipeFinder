537f8c03904a12cfadb4d86e09dd2218c00f1fe3
vivekmacrosoft <40669310+vivekmacrosoft@users.noreply.github.com>
2018-06-29 15:42:50 -0400

    ReciveImage Controller

--- RecipeFinder (BackEnd)/recipefinder/src/main/java/com/recipefinder/controller/RecipeController.java
+++ RecipeFinder (BackEnd)/recipefinder/src/main/java/com/recipefinder/controller/RecipeController.java
@@ -1,9 +1,23 @@
 package com.recipefinder.controller;
 
-import com.recipefinder.model.Recipe;
+import java.io.File;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.recipefinder.service.RecipeService;
+@RestController
 public class RecipeController {
-	
+	@Autowired
+	RecipeService recipeService;
 	
 	/**
 	 * @return
@@ -12,11 +26,26 @@ public class RecipeController {
 	 * Send recipe reponse to client
 	 * 
 	 */
-	public String getRecipe() {
+	@PostMapping("/upload")
+	public ResponseEntity<String> getRecipe(@RequestParam("file") MultipartFile file) {
 		
+		recipeService.getRecipe(file);
+		String message;
+		try {
+			
+ 
+			message = "You successfully uploaded " + file.getOriginalFilename() + "!";
+			return ResponseEntity.status(HttpStatus.OK).body(message);
+		} catch (Exception e) {
+			message = "FAIL to upload " + file.getOriginalFilename() + "!";
+			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+		}
 		
-		return null;
-		
+	}
+	
+	@GetMapping(value="/check")
+	public String checking() {
+		return "hello";
 	}
 	
 }
